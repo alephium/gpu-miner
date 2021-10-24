477a303d536b1a4cb8e095982403c5cd60ca2177
Cheng Wang <wachmc@gmail.com>
2021-10-24 21:56:20 +0200

    Minor

--- src/constants.h
+++ src/constants.h
@@ -4,7 +4,7 @@
 #define group_nums 4
 #define chain_nums 16
 #define max_gpu_num 1024
-#define parallel_mining_works_per_gpu 2
+#define parallel_mining_works_per_gpu 4
 #define max_worker_num (max_gpu_num * parallel_mining_works_per_gpu)
 #define mining_steps 5000
 
