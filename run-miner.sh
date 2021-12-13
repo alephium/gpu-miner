36fa4e6e81a3fa6e2d7f4bad21ef4f81c3888cc8
Cheng Wang <wachmc@gmail.com>
2021-12-13 14:32:19 +0100

    Update run-miner.sh

--- run-miner.sh
+++ run-miner.sh
@@ -2,24 +2,27 @@
 
 set -eu
 
+API_KEY="0000000000000000000000000000000000000000000000000000000000000000"
+
 if ! command -v jq &> /dev/null
 then
     echo "Installing jq"
     sudo apt install -y jq
 fi
 
-if ! node=$(curl --silent -X 'GET' "http://${2:-127.0.0.1}:12973/infos/self-clique"); then
+node=$(curl --silent -X 'GET' "http://${2:-127.0.0.1}:12973/infos/self-clique" -H "X-API-KEY: $API_KEY")
+if [ -z "$node" ]; then
     echo "Your full node is not running"
     exit 1
 fi
 
 synced=$(echo $node | jq '.synced')
 if [ -z "$synced" ] || [ "$synced" != "true" ]; then
-    echo "Your full node is not synced"
+    echo "Your full node is not synced or API key is not correct: $node"
     exit 1
 fi
 
-addresses=$(curl --silent -X 'GET' "http://${2:-127.0.0.1}:12973/miners/addresses" | jq '.addresses')
+addresses=$(curl --silent -X 'GET' "http://${2:-127.0.0.1}:12973/miners/addresses" -H "X-API-KEY: $API_KEY" | jq '.addresses')
 if [ -z "$addresses" ]; then
     echo "Miner addresses are not set"
     exit 1
