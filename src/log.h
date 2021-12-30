aa80026129e4e260753ee851dcb5f1b05a7cc361
lbqds <lbqds@outlook.com>
2021-12-30 12:12:49 +0800

    Add localtime_r for windows

--- src/log.h
+++ src/log.h
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <time.h>
 
+#ifdef _WIN32
+#define localtime_r(a, b) localtime_s(b, a)
+#endif
+
 #define LOG_TIMESTAMP(stream)                       \
-({                                                  \
+{                                                   \
     time_t now = time(NULL);                        \
     struct tm time;                                 \
     localtime_r(&now, &time);                       \
     char str[24];                                   \
     strftime(str, 24, "%Y-%m-%d %H:%M:%S", &time);  \
     fprintf(stream, "%s | ", str);                  \
-})
+}
 
 #define LOG_WITH_TS(stream, format, ...)            \
-({                                                  \
+{                                                   \
     LOG_TIMESTAMP(stream);                          \
     fprintf(stream, format, ##__VA_ARGS__);         \
-})
+}
 
 #define LOG_WITHOUT_TS(format, ...) fprintf(stdout, format, ##__VA_ARGS__);
 
